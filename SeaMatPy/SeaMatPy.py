92a4a861d14a6f30e8832ea4af1f8bfe3fdb9c27
OtherUser <otheruser@Stefanoss-Air.home>
2020-05-11 09:39:09 -0400

    Second

--- SeaMatPy/SeaMatPy.py
+++ SeaMatPy/SeaMatPy.py
@@ -5,7 +5,8 @@ def convert(my_name):
         my_name (str): person's name
     Returns:
         None
-    """
+        """
 
-            print(f"I'll convert a notebook for you some day, {my_name}.")
+        print(f"I'll convert a notebook for you some day, {my_name}.")
 
+        return None
